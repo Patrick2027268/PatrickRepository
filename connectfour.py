b42408616857d250c2058ac5ea00d957c448e79c
Patrick2027268 <2027268@sluh.org>
2025-04-28 11:26:52 -0500

    Update connectfour.py

--- connectfour.py
+++ connectfour.py
@@ -1,2 +1,21 @@
+board = [[0],[0],[0],[0],[0],[0],[0],[0]]
+
 for x in range(0,8):
     for y in range(0,8):
+        board[x].append(0)
+    print(board[x])
+
+for game in range(0,49):
+    column = int(input("Which column would you like to put in?"))
+    for y in range(0,7):
+        if board[column][y] == 0:
+           board[column][y] = 1
+           break
+    print(board)
+    column = int(input("Which column would you like to put in?"))
+    for y in range(0,7):
+        if board[column][y] == 0:
+           board[column][y] = 2
+           break
+    print(board)
+    
